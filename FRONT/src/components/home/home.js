5761513f372c8a02b0e7f6bf48abd4f542a8d65b
MatPerdigues <matperdigues@gmail.com>
2024-04-26 20:33:48 -0300

    V.26Abr24

--- FRONT/src/components/home/home.js
+++ FRONT/src/components/home/home.js
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
 import './home.css';
+import { FaTruckFast } from "react-icons/fa6";
+import { RiTeamFill } from "react-icons/ri";
 
 export default function Home(){
     return(
@@ -12,6 +14,17 @@ export default function Home(){
                     <div class='textoDescripcion'>
                         <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                     </div>
+                </section>
+                <section class='secCards'>
+                    <section class='card'>
+                        <div class='cardImg'></div>
+                    </section>
+                    <section class='card'>
+                        <div class='cardImg2'><FaTruckFast /></div>
+                    </section>
+                    <section class='card'>
+                        <div class='cardImg2' id='cardImg3'><RiTeamFill /></div>
+                    </section>
                 </section>
             </main>
 
