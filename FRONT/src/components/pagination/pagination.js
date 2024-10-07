82a77292da59712767575eaeb4a0a7858f8158f0
MatPerdigues <matperdigues@gmail.com>
2024-10-07 02:19:02 -0300

    V.06Oct24

--- FRONT/src/components/pagination/pagination.js
+++ FRONT/src/components/pagination/pagination.js
@@ -1,6 +1,8 @@
 import { Fragment, useEffect, useState } from 'react';
 import './pagination.css';
-import Marcas from '../marcas/marcas';
+
+import { GrNext } from "react-icons/gr";
+import { GrPrevious } from "react-icons/gr";
 const API = process.env.REACT_APP_API_URL;
 
 
@@ -8,8 +10,7 @@ const API = process.env.REACT_APP_API_URL;
 export default function Pagination(){
     
     const[marcas,setMarcas]=useState([]);
-    // const[pos,setPos]=useState(-100);
-    let pos = 0;
+
 
     const traerMarcas= async()=>{
         let marcas = await fetch(API+"/traerMarcas")   
@@ -20,6 +21,8 @@ export default function Pagination(){
         }
 
 
+      
+
     useEffect(()=>{
         traerMarcas();       
 
@@ -27,159 +30,115 @@ export default function Pagination(){
 
     
 
-    const pagination = (event)=>{  
-        if(event.currentTarget.id==='link2'){           
-            document.getElementById('containerItems').style.left='0%';
-        }
-        if(event.currentTarget.id==='link3'){           
-            document.getElementById('containerItems').style.left='-100%';
-        }
-        if(event.currentTarget.id==='link4'){           
-            document.getElementById('containerItems').style.left='-200%';
-        }     
-    }
 
                   
 
-    const opcCompra2=()=>{
+     const opcCompra2=(nombre)=>{
 
-        console.log(document.getElementById("pagMarcas").childElementCount);
+        
+        console.log(nombre);
+
 
     }
-
-
-
-      const pagination1 = (event)=>{ 
-
-
-        if(document.getElementById('containerMarcasPag').style.left==="0%"){
-
-
-
-        }else{
-
-            
-            pos = pos+100;
-            //     let pos = +100;
-            let posFinal = pos + '%';
-            //    // console.log(posFinal);  
-            
-            document.getElementById('containerMarcasPag').style.left=`${posFinal}`;  
-            //    console.log(posFinal); 
-            console.log(posFinal);
-        }
-        
-        
-      }
- 
-
-
-
-        const pagination2 = (event)=>{  
-
-            
-            pos = pos-100;
-             let posFinal = pos + '%';
-            // console.log(posFinal);
-            
-            
-     
-            document.getElementById('containerMarcasPag').style.left=`${posFinal}`;  
-
-
-
-            console.log(posFinal);
-            console.log(document.getElementById('containerMarcasPag').style.right);
-            console.log(document.getElementById('containerMarcasPag').style.left);
-
-        }
-
-
-
     
+    const distrMarcas = ()=>{ 
 
-      
-  
+
+        let idContainerImg=0;
+        let idContainerImgs = 0;
+        let carouselItem = 0;
+            
+        for(let x=0; x<marcas.length;x++){
+                
+            if(document.getElementById('containerImgs'+idContainerImgs).childElementCount<6){
+                if(document.getElementById("containerImg"+idContainerImg)===null){
+                    let containerImg = document.createElement('div');
+                    containerImg.setAttribute("class","containerImg");
+                    containerImg.setAttribute("id","containerImg"+ idContainerImg);
+                    document.getElementById('containerImgs'+idContainerImgs).appendChild(containerImg);  
+                    
+                    let imagenMarca = document.createElement('img');
+                    imagenMarca.setAttribute("src",marcas[x].imagen);
+                    imagenMarca.setAttribute("alt","img marca");
+                    imagenMarca.setAttribute("class","imgMarcas");
+                    imagenMarca.setAttribute("id",marcas[x].nombre);  
+                    document.getElementById("containerImg"+ idContainerImg).appendChild(imagenMarca);  
+                    document.getElementById(marcas[x].nombre).addEventListener("click", function(){opcCompra2(marcas[x].nombre)})
+                    
+                    idContainerImg++;
+                }
+            }else{
+
+                carouselItem++;
+                idContainerImgs++;
+                        
+
+                if(document.getElementById("carouselItem"+carouselItem)===null){                        
+                    let divCarousel = document.createElement('div');  
+                    divCarousel.setAttribute("class","carousel-item");
+                    divCarousel.setAttribute("id","carouselItem"+carouselItem);
+                    document.getElementById('carouselInner').appendChild(divCarousel); 
+                }
+
+                if(document.getElementById("containerImgs"+idContainerImgs)===null){                        
+                    let containerImgs = document.createElement('div');  
+                    containerImgs.setAttribute("class","containerImgs");
+                    containerImgs.setAttribute("id","containerImgs"+ idContainerImgs);
+                    document.getElementById("carouselItem"+carouselItem).appendChild(containerImgs); 
+                }
+
+                if(document.getElementById("containerImg"+idContainerImg)===null){
+                    let containerImg = document.createElement('div');
+                    containerImg.setAttribute("class","containerImg");
+                    containerImg.setAttribute("id","containerImg"+ idContainerImg);
+                    document.getElementById('containerImgs'+idContainerImgs).appendChild(containerImg);  
+                    
+                    let imagenMarca = document.createElement('img');
+                    imagenMarca.setAttribute("src",marcas[x].imagen);
+                    imagenMarca.setAttribute("alt","img marca");
+                    imagenMarca.setAttribute("class","imgMarcas");
+                    imagenMarca.setAttribute("id",marcas[x].nombre);  
+                    document.getElementById("containerImg"+ idContainerImg).appendChild(imagenMarca);  
+                    document.getElementById(marcas[x].nombre).addEventListener("click", function(){opcCompra2(marcas[x].nombre)})
+                    
+                    idContainerImg++;
+                }
+                
+                
+            }
+            }
+        }     
+
+        distrMarcas();
 
 
     return(
         <Fragment>
-            <section class='secPagination' id='secPagination'>
-                <nav aria-label="Page navigation example" class='containerNav'>
-                    <ul class="pagination" >
-                      <li onClick={pagination} class="page-item" id='link1'><button class="page-link">Previous</button></li>                      
-                      <li onClick={pagination} class="page-item" id='link2' active><a class="page-link" href="#">1</a></li>
-                      <li onClick={pagination} class="page-item" id='link3'><a class="page-link" href="#">2</a></li>
-                      <li onClick={pagination} class="page-item" id='link4'><a class="page-link" href="#">Next</a></li>
-                    </ul>
-                </nav>
-                <section class='containerItems' id='containerItems'> 
-                    <section class='items1' id='items1'>
-                        <div class='itemPage'></div>
-                        <div class='itemPage'></div>
-                        <div class='itemPage'></div>
-                        <div class='itemPage'></div>
-                    </section>
-                    <section class='items1'>
-                        <div class='itemPage2'></div>
-                        <div class='itemPage2'></div>
-                        <div class='itemPage2'></div>
-                        <div class='itemPage2'></div> 
-                    </section>
-                    <section class='items1'>
-                        <div class='itemPage3'></div>
-                        <div class='itemPage3'></div>
-                        <div class='itemPage3'></div>
-                        <div class='itemPage3'></div> 
-                    </section>
-                </section>
-            </section>
-
-            <section class='pagMarcas' id='pagMarcas'>
-
-                <section class='containerMarcasPag' id='containerMarcasPag'>                       
-                    {marcas.map((marca)=>{
-                        return <Marcas key={marca.id} info={marca} opcCompra2={opcCompra2}/>
-                    })}                                              
-                </section>
+        <section class='secPagination' id='secPagination'>
 
 
-                {/* <section class='marcasPag' id='marcasPag'>
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[0].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>                        
-                </section>
-                <section class='marcasPag' id='marcasPag2'>
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/> 
-                    <img src={marcas[1].imagen} alt="img marca" class='imgMarcasPag' id={marcas[0].nombre} onClick={opcCompra2}/>                        
-                </section>                 */}
-            </section>
+            <div id="carouselExample" class="carousel slide" >
+                <div class="carousel-inner" id='carouselInner'>                
+                    <div class="carousel-item active" id='carouselItem0'>
+                        <div class='containerImgs' id='containerImgs0'></div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev" id='flecha'>
+                    <GrPrevious id='flecha1'/>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next" id='flecha'>
 
-            <nav aria-label="Page navigation example" class='containerNav1'>
-                    <ul class="pagination" >
-                      <li onClick={pagination1} class="page-item" id='link11'><button class="page-link">Previous</button></li>                     
+                    <GrNext id='flecha1'/>
 
-                      <li onClick={pagination2} class="page-item" id='link14'><button class="page-link" >Next</button></li>
-                    </ul>
-                </nav>
-                    
-                    
+                </button>
+            </div>
 
-
-
-
-
+        </section>
 
 
         </Fragment>
-    )
+)
+
 }
 
 
