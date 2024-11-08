0d6e5e783a8f0713cc6e50e4e7e7253d73f20beb
MatPerdigues <matperdigues@gmail.com>
2024-11-08 01:07:35 -0300

    V.07Nov24

--- FRONT/src/components/cardDetalles/cardDetalles.js
+++ FRONT/src/components/cardDetalles/cardDetalles.js
@@ -4,17 +4,18 @@ import './cardDetalles.css';
 
 export default function CardDetalles({info,listProd}){
 
-    const prueba = ()=>{
-        console.log(listProd);
-       }
-
-
+    let ind = listProd.findIndex(obj=> obj.id===info.id);
 
     return(
         <Fragment>
-            <p>{info.cantidad}</p>
-            {/* <p>{listProd.marca}</p> */}
+
+
+            <table class='tablaDetalle' id='tablaDetalle'>
+                <td class='tdCardDetalle1' id='tdCardDetalle1'>{listProd[ind].descripcion}</td>
+                <td class='tdCardDetalle2' id='tdCardDetalle2'>{listProd[ind].marca}</td>
+                <td class='tdCardDetalle3' id='tdCardDetalle3'>{info.cantidad}</td>
+            </table>
           
         </Fragment>
     )
-} 
+}   
--- FRONT/src/components/cardPedidos/cardPedidos.js
+++ FRONT/src/components/cardPedidos/cardPedidos.js
@@ -1,102 +1,51 @@
-import { Fragment,useEffect } from 'react'
+import { Fragment } from 'react'
 import './cardPedidos.css'
 import CardDetalles from '../cardDetalles/cardDetalles';
+import { LiaAngleUpSolid } from "react-icons/lia";
 
 export default function CardPedidos({info,listProd}){
 
     const productos = JSON.parse(info.pedido); 
 
-    let objetoArray = [];
+    const onHover=()=>{
 
-   const prueba = ()=>{
-    console.log(listProd);
-   }
-    
-    // mostrarProd =()=>{
-
-    //     for(let x=0; x<productos.length; x++){
-    //         let ind = listProd.findIndex(obj=> obj.id===productos[x].id);
-    //         console.log(productos[x].id);
-    //         console.log(productos[x].cantidad);
-    //         console.log(listProd[ind].descripcion);
-    //         console.log(listProd[ind].marca);
+        document.getElementById("infoPedido" + info.id).style.backgroundColor='rgb(255, 165, 0, 0.2)';
+    }
 
 
-    //         const parent1 = document.getElementById("detalleProd"+info.id);
-    //         const child1 = document.getElementById('tabDetalle'+info.id+"-"+productos[x].id);
-    //         if(parent1.contains(child1)===false){
-    //             let tableDetalle = document.createElement('table');
-    //             tableDetalle.setAttribute('class','tabDetalle');
-    //             tableDetalle.setAttribute('id','tabDetalle'+info.id+"-"+productos[x].id);
-    //             document.getElementById("detalleProd"+info.id).appendChild(tableDetalle);
-                
-    //             let tdDetalle1 = document.createElement('td');
-    //             tdDetalle1.setAttribute('class','tdDetalle1');
-    //             tdDetalle1.setAttribute('id','tdDetalle1'+info.id+"-"+productos[x].id);
-    //             tdDetalle1.innerHTML=listProd[ind].descripcion;
-    //             document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle1);
-                
-    //             let tdDetalle3 = document.createElement('td');
-    //             tdDetalle3.setAttribute('class','tdDetalle3');
-    //             tdDetalle3.setAttribute('id','tdDetalle3'+info.id+"-"+productos[x].id);
-    //             tdDetalle3.innerHTML=listProd[ind].marca;
-    //             document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle3);
+    const outHover=()=>{
+        document.getElementById("infoPedido" + info.id).style.backgroundColor='white';
+    }
+
+
+    const expandirInfo = ()=>{
+        document.getElementById("infoPedido" + info.id).style.backgroundColor='white';
+        document.getElementById("infoPedido" + info.id).style.cursor='default';
+        document.getElementById("infoPedido" + info.id).style.height='auto';
+        document.getElementById("tabPedidos" + info.id).style.height='auto';
+        document.getElementById("infoPedido" + info.id).style.backgroundColor='rgb(255, 165, 0, 0.2)';
+        document.getElementById("infoPedido" + info.id).onmouseover = function() {onHover()};
+        document.getElementById("infoPedido" + info.id).onmouseout = function() {outHover()};
+
+
+    }
+
+
+    const contraerInfo=()=>{
+        document.getElementById("infoPedido" + info.id).style.cursor='pointer';
+        document.getElementById("infoPedido" + info.id).style.height='50px';
+        document.getElementById("tabPedidos" + info.id).style.height='50px';
+        document.getElementById("infoPedido" + info.id).onmouseover = function() {onHover()};
+        document.getElementById("infoPedido" + info.id).onmouseout = function() {outHover()};
+
         
-    //             let tdDetalle2 = document.createElement('td');
-    //             tdDetalle2.setAttribute('class','tdDetalle2');
-    //             tdDetalle2.setAttribute('id','tdDetalle2'+info.id+"-"+productos[x].id);
-    //             tdDetalle2.innerHTML=productos[x].cantidad;
-    //             document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle2);
-            
-    //         }
-    // }
-
-   
-       
-        // for(let x=0; x<productos.length; x++){
-        //     for(let y=0; y<listProd.length; y++){
-        //         if(productos[x].id===listProd[y].id)
-        //             console.log(productos[x].id + listProd[y].descripcion + productos[x].cantidad);
-        //             const parent1 = document.getElementById("detalleProd"+info.id);
-        //             const child1 = document.getElementById('tabDetalle'+info.id+"-"+productos[x].id);
-
-        //             if(parent1.contains(child1)===false){
-        //                 let tableDetalle = document.createElement('table');
-        //                 tableDetalle.setAttribute('class','tabDetalle');
-        //                 tableDetalle.setAttribute('id','tabDetalle'+info.id+"-"+productos[x].id);
-        //                 document.getElementById("detalleProd"+info.id).appendChild(tableDetalle);
-
-        //                 let tdDetalle1 = document.createElement('td');
-        //                 tdDetalle1.setAttribute('class','tdDetalle1');
-        //                 tdDetalle1.setAttribute('id','tdDetalle1'+info.id+"-"+productos[x].id);
-        //                 tdDetalle1.innerHTML=listProd[y].descripcion;
-        //                 document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle1);
-
-        //                 let tdDetalle3 = document.createElement('td');
-        //                 tdDetalle3.setAttribute('class','tdDetalle3');
-        //                 tdDetalle3.setAttribute('id','tdDetalle3'+info.id+"-"+productos[x].id);
-        //                 tdDetalle3.innerHTML=listProd[y].marca;
-        //                 document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle3);
-                
-        //                 let tdDetalle2 = document.createElement('td');
-        //                 tdDetalle2.setAttribute('class','tdDetalle2');
-        //                 tdDetalle2.setAttribute('id','tdDetalle2'+info.id+"-"+productos[x].id);
-        //                 tdDetalle2.innerHTML=productos[x].cantidad;
-        //                 document.getElementById('tabDetalle'+info.id+"-"+productos[x].id).appendChild(tdDetalle2);
-                    
-        //             }
-                    
-        //     }
-        // }
-
-
-    // }
+    }
 
     
     return(
         <Fragment>
-            <section class='infoPedido'>
-                <table class='tabPedidos'>
+            <section class='infoPedido' id={'infoPedido' + info.id}>
+                <table class='tabPedidos' id={"tabPedidos" + info.id} onClick={expandirInfo}>
                     <td class='tdPedidosAdmin' id='numPedidos'>{info.id}</td>
                     <td class='tdPedidosAdmin' id='nomPedidos'>{info.nombre}</td>
                     <td class='tdPedidosAdmin' id='totPedidos'>${info.total}</td>
@@ -115,6 +64,8 @@ export default function CardPedidos({info,listProd}){
                         return <CardDetalles key={producto.id} info={producto} listProd={listProd}/>
                     })} 
                 </div>
+                <div class='upIcon'><LiaAngleUpSolid class='upIcon2' onClick={contraerInfo}/></div>
+                
             </section>
             
         </Fragment>
